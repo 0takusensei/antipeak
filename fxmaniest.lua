e5034d06239456d074d5aa540404f4605cbd9346
Otaku Sensei <61647038+0takusensei@users.noreply.github.com>
2024-01-06 17:01:57 +0600

    Add files via upload

---
+++ fxmaniest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'OtakuSensei'
+version '1.0'
+description 'A script that allows you to stop abuse peak and shoot from corner'
+
+
+client_scripts {
+    'client.lua'
+}
